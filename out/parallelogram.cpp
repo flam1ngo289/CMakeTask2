0cc287efdc7f5abf05eec293dfadac37e667ab25
flam1ngo <kris.s.03@mail.ru>
2024-03-20 20:22:05 +0300

    Initial commit

---
+++ out/parallelogram.cpp
@@ -0,0 +1,5 @@
+#include "parallelogram.hpp"
+
+Parallelogram::Parallelogram(int side1, int side2, int angle1, int angle2) : Quadrangle(side1, side2, side1, side2, angle1, angle2, angle1, angle2) {
+    name = "ֿאנאככוכמדנאלל";
+}
